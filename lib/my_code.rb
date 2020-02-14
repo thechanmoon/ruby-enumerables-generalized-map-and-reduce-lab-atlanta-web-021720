6fa18e9515c7eb39ac6b7246ac2bf675fcccad93
CHANMOON PARK <thechanmoon@gmail.com>
2020-02-14 17:21:13 +0000

    Done.

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,26 @@
 # Your Code Here
+
+def map(s)
+  retVal = []
+  i = 0
+  while i < s.length
+    retVal.push(yield(s[i]))
+    i += 1
+  end
+  return retVal
+end
+
+def reduce(s, sp=nil)
+  if sp
+    retVal = sp
+    i = 0
+  else
+    retVal = s[0]
+    i = 1
+  end
+  while i < s.length
+    retVal = yield(retVal, s[i])
+    i += 1
+  end
+  return retVal
+end
